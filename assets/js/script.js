d728d0df775d19d04275238e454d8ac28eeee95a
SMakela <73409799+smakela13@users.noreply.github.com>
2021-03-15 22:30:56 -0400

    Adding Dwayne's updates to mapbox and leafletjs as well as the corrected search-html to map and styles can work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,62 @@
-// search parameters needed
-// var mymap = L.map('#mapid').setView([51.505, -0.09], 13);
-// pk.eyJ1IjoiZGhvdjkyIiwiYSI6ImNrbTd6eHoxNzEyeGwydXMzcWFybHp6MmgifQ.zZ3yEJs-XX_N5SZIPaiaBw
-// leaflet api token
+var searchZip = document.querySelector("#zipcode");
+var searchBtn = document.querySelector("#search-button");
+var result;
 
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoiZGhvdjkyIiwiYSI6ImNrbTd6eHoxNzEyeGwydXMzcWFybHp6MmgifQ.zZ3yEJs-XX_N5SZIPaiaBw}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'your.mapbox.access.token'
-// }).addTo(mymap);
-// var map = L.map('mapid', {
-//     center: [51.505, -0.09],
-//     zoom: 13
-// });
-var map = L.map('mapid').setView([51.505, -0.09], 15);
-// L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-//     {
-//       attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-//       maxZoom: 17,
-//       minZoom: 9
-//     }).addTo(map);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiZGhvdjkyIiwiYSI6ImNrbTd6eHoxNzEyeGwydXMzcWFybHp6MmgifQ.zZ3yEJs-XX_N5SZIPaiaBw'
+
+
+function getZipCode(event) {
+    event.preventDefault();
+    var zipText = searchZip.value;
+
+    var mapUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + zipText + ".json?access_token=pk.eyJ1IjoiZGhvdjkyIiwiYSI6ImNrbTd6eHoxNzEyeGwydXMzcWFybHp6MmgifQ.zZ3yEJs-XX_N5SZIPaiaBw";
+
+    console.log(mapUrl);
+
+    fetch(mapUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            result = data;
+        });
+
+}
+
+
+searchBtn.addEventListener("click", getZipCode);
+
+{/* <a class="btn" href="URL goes in here">Submit</a> */ }
+// fetch("https://api.mapbox.com/geocoding/v5/mapbox.postcode/+ziptxt+.json?access_token=pk.eyJ1IjoiZGhvdjkyIiwiYSI6ImNrbTd6eHoxNzEyeGwydXMzcWFybHp6MmgifQ.zZ3yEJs-XX_N5SZIPaiaBw")
+
+
+
+var map = L.map('map').setView([-78.94, 35.9], 15);
+L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+    {
+        attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+        maxZoom: 17,
+        minZoom: 9
     }).addTo(map);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZGhvdjkyIiwiYSI6ImNrbTd6eHoxNzEyeGwydXMzcWFybHp6MmgifQ.zZ3yEJs-XX_N5SZIPaiaBw'
+}).addTo(map);
+
+var marker = L.marker([-78.94, 35.9]).addTo(map);
+marker.bindPopup("<b>Hey Buddy Finder!</b><br>I am a popup.").openPopup();
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
